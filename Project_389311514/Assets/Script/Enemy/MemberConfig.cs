af1e2b8f76deb237c6d9d5518b94b37647452911
DESKTOP-IV0RAG0\hamdi <h.hudhaidh@hotmail.com>
2018-09-04 11:58:49 +0400

    adding forgotten files

---
+++ Project_389311514/Assets/Script/Enemy/MemberConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemberConfig : MonoBehaviour
+{
+    public float maxFOV = 180;
+    public float maxAcceleration;
+    public float maxVelocity;
+
+    //wander
+    public float wanderJitter;
+    public float wanderRadius;
+    public float wanderDistance;
+    public float wanderPriority;
+
+    //cohesion
+    public float cohesionRadius;
+    public float cohesionPriority;
+
+    //alignment
+    public float alignmentRadius;
+    public float alignmentPriority;
+
+    //seperation 
+    public float seperationRadius;
+    public float seperationPriority;
+
+    //avoidance
+    public float avoidanceRadius;
+    public float avoidancePriority;
+
+    //follow
+    public float followRadius;
+    public float followPriority;
+}
---
+++ Project_389311514/Assets/Script/Enemy/Point.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    public float accShortDist;
+    public Transform from;
+    public bool visited;
+
+	void Start ()
+    {
+        accShortDist = float.MaxValue;
+        visited = false;
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
